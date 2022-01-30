67018b467b409d4616c983ff61fc658a17b57325
jrayner <tombstoneman27@gmail.com>
2022-01-30 23:56:50 +0100

    WIP: adding error handeling to baseVector function

--- app/Line.hs
+++ app/Line.hs
@@ -1,6 +1,8 @@
 module Line where
 
-import Data.Vector ( (!), findIndex, Vector )
+import Control.Monad.Except
+import Data.Vector (Vector, findIndex, (!))
+import GHC.Base (undefined)
 
 data Line a = Line
   { normalVector :: Vector a, -- a normal vector is orthagonal vector to the line
@@ -19,9 +21,9 @@ intersection :: Line a -> Line a -> Either String (Vector a)
 intersection = undefined
 
 baseVector :: Line a -> Either String (Vector a)
-baseVector (Line v c) = undefined
-   where initial_index = findIndex (/= 0) v
-         initial_coefficient = v ! initial_index
-         
-         
-         
+baseVector (Line v c) = case initial_index of
+  Nothing -> Left "Zero vector can not be a normal vector for a line"
+  (Just a) -> undefined
+  where
+    initial_index = findIndex (/= 0) v
+    initial_coefficient = v ! initial_index
